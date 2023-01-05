e46053ab4a25c06245cba2a40f35f554c52d2690
gosgjung <gosgjung@gmail.com>
2023-01-05 17:28:17 +0900

    함수형프로그래밍

---
+++ 함수형 프로그래밍/example/fp-study/src/main/java/io/gosgjung/study/fpstudy/FpStudyApplication.java
@@ -0,0 +1,13 @@
+package io.gosgjung.study.fpstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpStudyApplication.class, args);
+    }
+
+}
---
+++ 함수형 프로그래밍/example/fp-study/src/test/java/io/gosgjung/study/fpstudy/FpStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gosgjung.study.fpstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
